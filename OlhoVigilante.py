2410748ba3338f997077009ae8f88ba205e056d6
Magno Emanoel <magno.emanoel@unimedmaranhaodosul.coop.br>
2023-12-26 14:58:25 -0300

    Atualizando código em OlhoVigilante.py

--- OlhoVigilante.py
+++ OlhoVigilante.py
@@ -3,22 +3,29 @@ import pyautogui
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+import pygetwindow as gw
+import os  # Importe o módulo os
 
-# Função para minimizar todas as janelas do Chrome
-def minimize_chrome_windows():
-    for w in pyautogui.getAllWindows():
-        if 'chrome' in w.title.lower():  # Verifica se 'chrome' está no título da janela
-            w.minimize()
+# Função para minimizar todas as janelas, exceto a barra de tarefas
+def minimize_all_windows_except_taskbar():
+    for window in gw.getAllTitles():
+        if window != "Detecção de Movimento" and window != "Barra de Tarefas":  # Adicione "Barra de Tarefas" como exceção
+            gw.getWindowsWithTitle(window)[0].minimize()
 
 # Função para fechar o programa
 def close_program():
     root.quit()
 
+# Adicione este código para corrigir o problema da tela preta com PyInstaller
+import sys
+if getattr(sys, 'frozen', False):
+    os.chdir(sys._MEIPASS)
+
 # Inicializa o subtrator de fundo MOG2
 fgbg = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=16, detectShadows=True)
 
 # Inicializa a captura de vídeo da webcam
-video_stream = cv2.VideoCapture(0)
+video_stream = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Cria a janela principal da interface gráfica
 root = tk.Tk()
@@ -31,6 +38,10 @@ close_button.pack()
 # Cria um rótulo para exibir o frame da webcam
 label = tk.Label(root)
 label.pack()
+
+# Função para minimizar todas as janelas quando o movimento é detectado, exceto a barra de tarefas
+def minimize_all_programs_except_taskbar():
+    minimize_all_windows_except_taskbar()
 
 def update_frame():
     ret, frame = video_stream.read()
@@ -58,9 +69,9 @@ def update_frame():
         label.img = img
         label.configure(image=img)
 
-        # Minimiza as janelas do Chrome se movimento for detectado
+        # Minimiza todas as janelas, exceto a barra de tarefas, se movimento for detectado
         if movement_detected:
-            minimize_chrome_windows()
+            minimize_all_programs_except_taskbar()
 
     # Atualiza a interface gráfica
     root.after(10, update_frame)
